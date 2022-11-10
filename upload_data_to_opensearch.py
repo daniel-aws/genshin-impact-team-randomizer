e99eb18614a5bd84f63c8299aff1955b08540475
Daniel Lee <daniellee0217@yahoo.com>
2022-11-10 14:12:09 -0500

    Removing Opensearch integration since it is an internal feature, and moved csv to: https://github.com/daniel-aws/genshin-spiral-abyss-teams-compilation

--- upload_data_to_opensearch.py
+++ upload_data_to_opensearch.py
@@ -16,26 +16,6 @@ if not hashseed:
 
 print("Starting, please wait until complete")
 
-with open("genshinTeamsNamed.csv",'r') as f:
-    with open("./src/data/teampresets.ts",'w') as f1:
-        f.readline() # skip header line
-        f1.write("export const teamPresets = [\n")
-        for line in f:
-            line = '\t[' + line
-
-            for i in data:
-                if i["id"] != 39:
-                    line = line.replace(i["shortName"], str(i["id"]))
-
-            # Get rid of the trailing newline (if any).
-            line = line.rstrip('\n')
-            line += '],\n'
-            f1.write(line)
-        f1.write("];")
-    f1.close()
-f.close()
-
-
 host = os.getenv('OPENSEARCH_ENDPOINT')
 port = 443
 region = 'us-east-1'
