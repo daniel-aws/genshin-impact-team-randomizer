972d281b56b04813108ad0b0825efd130e297458
Daniel Lee <daniellee0217@yahoo.com>
2023-02-02 15:41:47 -0500

    Fix

--- convert_csv_to_array.py
+++ convert_csv_to_array.py
@@ -24,52 +24,6 @@ with open("genshinTeamsNamed.csv",'r') as f:
     f1.close()
 f.close()
 
-print("Starting 3.2 First Half List, please wait until complete")
-
-with open("genshinTeamsNamed33FirstHalf.csv",'r') as f:
-    with open("./src/data/teampresets33FirstHalf.ts",'w') as f1:
-        f.readline() # skip header line
-        f1.write("export const teamPresets33FirstHalf = [\n")
-        for line in f:
-            line = '\t[' + line
-
-            for i in data:
-                if i["id"] != 39:
-                    line = line.replace(i["shortName"], str(i["id"]))
-
-            # Get rid of the trailing newline (if any).
-            line = line.rstrip('\n')
-            line += '],\n'
-            f1.write(line)
-        f1.write("];")
-    f1.close()
-f.close()
-
-jsonFile.close()
-
-print("Starting 3.2 Second Half List, please wait until complete")
-
-with open("genshinTeamsNamed33SecondHalf.csv",'r') as f:
-    with open("./src/data/teampresets33SecondHalf.ts",'w') as f1:
-        f.readline() # skip header line
-        f1.write("export const teamPresets33SecondHalf = [\n")
-        for line in f:
-            line = '\t[' + line
-
-            for i in data:
-                if i["id"] != 39:
-                    line = line.replace(i["shortName"], str(i["id"]))
-
-            # Get rid of the trailing newline (if any).
-            line = line.rstrip('\n')
-            line += '],\n'
-            f1.write(line)
-        f1.write("];")
-    f1.close()
-f.close()
-
-jsonFile.close()
-
 print("Job Complete")
 
 exit(0)
